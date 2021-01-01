dc4cbc416fa8acc465dae2112aa0c4e48db5d8db
Praneeth <18praneeth@gmail.com>
2021-01-01 20:49:56 +0530

    Login views

--- Praneeth/scl-doubtology/udayagiri/user/views.py
+++ Praneeth/scl-doubtology/udayagiri/user/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth, messages
 
 def user_login(request):
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.warning(request, "You are logged in")
+        else:
+            messages.warning(request, "Invalid credentials")
+            return redirect('login')
+
     return render(request, 'auth_templates/login.html')
 
 def user_register(request):
