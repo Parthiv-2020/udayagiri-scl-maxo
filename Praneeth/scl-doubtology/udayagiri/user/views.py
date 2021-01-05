80e11d2a289d1f0400033136b95e73cb3e4a588b
Praneeth <18praneeth@gmail.com>
2021-01-05 18:51:12 +0530

    Logout of Users

--- Praneeth/scl-doubtology/udayagiri/user/views.py
+++ Praneeth/scl-doubtology/udayagiri/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth, messages
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
 def user_login(request):
     if request.method == 'POST':
@@ -54,32 +55,6 @@ def user_profile(request):
 def user_forgot_password(request):
     return render(request, 'auth_templates/forgot_password.html')
 
-
-# def register(request):
-#     if request.method == 'POST':
-#         firstname = request.POST['firstname']
-#         lastname = request.POST['lastname']
-#         username = request.POST['username']
-#         email = request.POST['email']
-#         password = request.POST['password']
-#         confirm_password = request.POST['confirm_password']
-
-#         if password == confirm_password:
-#             if User.objects.filter(username=username).exists():
-#                 messages.warning(request, 'Username exists')
-#                 return redirect('register')
-#             else:
-#                 if User.objects.filter(email=email).exists():
-#                     messages.warning(request, 'email already exists')
-#                     return redirect('register')
-#                 else:
-#                     user = User.objects.create_user(
-#                         first_name=firstname, last_name=lastname, username=username, email=email, password=password)
-#                     user.save()
-#                     messages.success(request, 'Account created successfully')
-#                     return redirect('login')
-#         else:
-#             messages.warning(request, 'Password do not match')
-#             return redirect('register')
-
-#     return render(request, 'accounts/register.html')
+def user_logout(request):
+    logout(request)
+    return redirect('home')
