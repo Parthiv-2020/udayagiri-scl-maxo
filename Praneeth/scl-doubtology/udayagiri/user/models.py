921ad6c8a972f2b7860f9c8d4f050ccef30235da
Praneeth <18praneeth@gmail.com>
2021-01-05 19:02:48 +0530

    Registration

--- Praneeth/scl-doubtology/udayagiri/user/models.py
+++ Praneeth/scl-doubtology/udayagiri/user/models.py
@@ -6,6 +6,13 @@ ROLE = (
     ('Student', 'Student')
 )
 
+class Role(models.Model):
+    Role = models.CharField(max_length=30)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='media/profile_pics/%Y/%m/')
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+
+
