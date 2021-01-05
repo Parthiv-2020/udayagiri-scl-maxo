dc6e7fad1afe09f5bd1e766f898c42e213bea71d
Praneeth <18praneeth@gmail.com>
2021-01-05 19:22:36 +0530

    Models.py error fixed

--- Praneeth/scl-doubtology/udayagiri/user/models.py
+++ Praneeth/scl-doubtology/udayagiri/user/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-ROLE = (
-    ('Teacher', 'Teacher'),
-    ('Student', 'Student')
-)
-
 class Role(models.Model):
     role = models.CharField(max_length=30)
 
@@ -18,6 +13,6 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='media/profile_pics/%Y/%m/')
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
 
-    # def __str__(self):
-    #     return 'Praneet'
+    def __str__(self):
+        return f'{user}'
     
