d7e5559f8ff983b14842f56402a6c3cd24cb4dae
Praneeth <18praneeth@gmail.com>
2021-01-05 19:49:53 +0530

    Removed extra folders

--- ajay-code2/maxo/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-	return render(request,'maxo/home.html')
-	
-def about(request):
-	return render(request,'maxo/about.html')
