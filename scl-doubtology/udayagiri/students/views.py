8040ff1c0ca26bd9789616da7246841b31e225dc
Praneeth <18praneeth@gmail.com>
2021-01-07 22:18:19 +0530

    Udated student model

--- scl-doubtology/udayagiri/students/views.py
+++ scl-doubtology/udayagiri/students/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Student
+from django.contrib import messages
 
-# Create your views here.
+
+def student(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        tuber_id = request.POST['tuber_id']
+        tuber_name = request.POST['tuber_name']
+        city = request.POST['city']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        state = request.POST['state']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+
+        student = Student(first_name=first_name, last_name=last_name, tuber_id=tuber_id, tuber_name=tuber_name,
+                              city=city, phone=phone, email=email, state=state, message=message, user_id=user_id)
+        student.save()
+        messages.success(request, 'Thanks for reaching out!')
+        return redirect('home')
