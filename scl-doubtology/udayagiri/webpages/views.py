99d6240253583a3a53ee2d72de2d4ea295fff8a9
Praneeth <18praneeth@gmail.com>
2021-01-08 23:32:46 +0530

    Description in teacher detail is updated

--- scl-doubtology/udayagiri/webpages/views.py
+++ scl-doubtology/udayagiri/webpages/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Contact
 from django.contrib import messages
-from django.core.mail import send_mail
 
 def home(request):
     return render(request, 'webpages/home.html')
@@ -11,27 +9,27 @@ def teacher(request):
 
 
 def contact(request):
-	if request.POST:
-        name = request.POST['name']
-        email = request.POST['email']
-        subject = request.POST['subject']
-        comment = request.POST['message']
-        comment = name + " with the email, " + email + ", sent the following message:\n\n" + comment
-        send_mail(subject, comment, None, ['18praneeth@gmail.com'])
-        message = Contact()
-        message.name = name
-        message.email = email
-        message.subject = subject
-        message.comments = comment
-        message.save()
-        if email:
-            messages.success(request, 'Thank you for showing interest in '
-                                      'the ABC, We will contact you '
-                                      'within '
-                                      '24hrs.')
-        else:
-            pass
-        return redirect(request.META['HTTP_REFERER'])
-    else:
-        return render(request, 'webpages/contact.html',{})
+	# if request.POST:
+    #     name = request.POST['name']
+    #     email = request.POST['email']
+    #     subject = request.POST['subject']
+    #     comment = request.POST['message']
+    #     comment = name + " with the email, " + email + ", sent the following message:\n\n" + comment
+    #     send_mail(subject, comment, None, ['18praneeth@gmail.com'])
+    #     message = Contact()
+    #     message.name = name
+    #     message.email = email
+    #     message.subject = subject
+    #     message.comments = comment
+    #     message.save()
+    #     if email:
+    #         messages.success(request, 'Thank you for showing interest in '
+    #                                   'the ABC, We will contact you '
+    #                                   'within '
+    #                                   '24hrs.')
+    #     else:
+    #         pass
+    #     return redirect(request.META['HTTP_REFERER'])
+    # else:
+    return render(request, 'webpages/contact.html',{})
     
