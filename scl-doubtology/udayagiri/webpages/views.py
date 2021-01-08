a9eaeeb5b2dc48e5a2c3b6c266e57d19e277e7a2
Praneeth <18praneeth@gmail.com>
2021-01-08 23:59:02 +0530

    Contact admin

--- scl-doubtology/udayagiri/webpages/views.py
+++ scl-doubtology/udayagiri/webpages/views.py
@@ -1,35 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import Contact
 
 def home(request):
     return render(request, 'webpages/home.html')
 
-def teacher(request):
-    return render(request, 'webpages/teacher.html')
-
 
 def contact(request):
-	# if request.POST:
-    #     name = request.POST['name']
-    #     email = request.POST['email']
-    #     subject = request.POST['subject']
-    #     comment = request.POST['message']
-    #     comment = name + " with the email, " + email + ", sent the following message:\n\n" + comment
-    #     send_mail(subject, comment, None, ['18praneeth@gmail.com'])
-    #     message = Contact()
-    #     message.name = name
-    #     message.email = email
-    #     message.subject = subject
-    #     message.comments = comment
-    #     message.save()
-    #     if email:
-    #         messages.success(request, 'Thank you for showing interest in '
-    #                                   'the ABC, We will contact you '
-    #                                   'within '
-    #                                   '24hrs.')
-    #     else:
-    #         pass
-    #     return redirect(request.META['HTTP_REFERER'])
-    # else:
-    return render(request, 'webpages/contact.html',{})
+    if request.POST:
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        comment = request.POST['message']
+        message = Contact()
+        message.name = name
+        message.email = email
+        message.subject = subject
+        message.comments = comment
+        message.save()
+        return redirect('contact')
+    else:
+        return render(request, 'webpages/contact.html',{})
     
